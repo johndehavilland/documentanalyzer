77a201df07ef5b14e1ddf613d6cd84183c67d948
John de Havilland <johndehavilland@gmail.com>
2018-05-08 17:59:32 -0400

    updated to remove hardcoded params

--- DocExtract/EnricherFunction/Config.cs
+++ DocExtract/EnricherFunction/Config.cs
@@ -11,28 +11,28 @@ namespace EnricherFunction
         /**************  UPDATE THESE CONSTANTS WITH YOUR SETTINGS  **************/
 
         // Azure Blob Storage used to store extracted page images
-        public static string IMAGE_AZURE_STORAGE_ACCOUNT_NAME = "jdhdocstrg";
-        public static string IMAGE_BLOB_STORAGE_ACCOUNT_KEY = "yf6ArvGRNz6fSg5dT5auY3hhgAcyRxV+8AiDhdNW4PVqqVsxMw42bQVVuiwrF4ExiQIL6fdmWMfuyBx8RyjddA==";
+        public static string IMAGE_AZURE_STORAGE_ACCOUNT_NAME = Environment.GetEnvironmentVariable("IMAGE_AZURE_STORAGE_ACCOUNT_NAME");
+        public static string IMAGE_BLOB_STORAGE_ACCOUNT_KEY = Environment.GetEnvironmentVariable("IMAGE_BLOB_STORAGE_ACCOUNT_KEY");
 
         // Cognitive Services Vision API used to process images
-        public static string VISION_API_KEY = "f2568fd765d440c498a671f7c3a94646";
+        public static string VISION_API_KEY = Environment.GetEnvironmentVariable("VISION_API_KEY");
         // The region URL base should match where you deployed your cognitive service to.  default is westus.
         // For list of region urls see https://westus.dev.cognitive.microsoft.com/docs/services/56f91f2d778daf23d8ec6739/operations/56f91f2e778daf14a499e1fa
-        public static string VISION_API_REGION = "eastus2.api.cognitive.microsoft.com";
+        public static string VISION_API_REGION = Environment.GetEnvironmentVariable("VISION_API_REGION");
 
         // Cognitive Entity Linking Service
-        public static string ENTITY_LINKING_API_KEY = "bfd3b050ac194385b1cbaf7d5faec2b3";
-        public static string ENTITY_LINKING_API_ENDPOINT = "westus.api.cognitive.microsoft.com";
+        public static string ENTITY_LINKING_API_KEY = Environment.GetEnvironmentVariable("ENTITY_LINKING_API_KEY");
+        public static string ENTITY_LINKING_API_ENDPOINT = Environment.GetEnvironmentVariable("ENTITY_LINKING_API_ENDPOINT");
 
         // Azure Search service used to index documents
-        public static string AZURE_SEARCH_SERVICE_NAME = "jdhdocsrch";
-        public static string AZURE_SEARCH_ADMIN_KEY = "DE6DCEEC51DAC5B8F5B9099D1E5ED78F";
+        public static string AZURE_SEARCH_SERVICE_NAME = Environment.GetEnvironmentVariable("AZURE_SEARCH_SERVICE_NAME");
+        public static string AZURE_SEARCH_ADMIN_KEY = Environment.GetEnvironmentVariable("AZURE_SEARCH_ADMIN_KEY");
 
         /*************************************************************************/
 
         // settings you can change if you want but the defaults should work too
-        public static string IMAGE_BLOB_STORAGE_CONTAINER = "images";
+        public static string IMAGE_BLOB_STORAGE_CONTAINER = Environment.GetEnvironmentVariable("IMAGE_BLOB_STORAGE_CONTAINER");
         public const string LIBRARY_BLOB_STORAGE_CONTAINER = "imagedocs";
-        public static string AZURE_SEARCH_INDEX_NAME = "docs";
+        public static string AZURE_SEARCH_INDEX_NAME = Environment.GetEnvironmentVariable("AZURE_SEARCH_INDEX_NAME");
     }
 }
